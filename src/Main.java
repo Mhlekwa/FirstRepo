057affaabf3bd7433acf285c1efbb44d6ac3c64b
Mhlekwa <mhlekwapillarqhayisa@gmail.com>
2024-05-10 14:27:40 +0200

    First Change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Your name");
+
+        String name = input.next();
+
+        System.out.println("Welcome " + name);
+
+
         System.out.println("Hello world!");
     }
 }
